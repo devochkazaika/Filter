8362e3d48c6974c43a4e11f5918f88465283b2aa
Panarin Ilya <cccthafcdh@gmail.com>
2024-01-14 19:51:40 +0700

    first_push

---
+++ src/main/java/com/filter/App.java
@@ -0,0 +1,43 @@
+package com.filter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+
+ 
+ 
+public class App 
+{
+    public static void reader(){
+        try {
+            File file = new File("files/file.txt");
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main( String[] args )
+    {
+        FileProcessing t = new FileProcessing("files/file.txt");
+
+    }
+}
